9e494666d42b6a5288e53355bc36803ae43fae8e
mrtcnylmz <mert9710@gmail.com>
2022-11-02 16:48:44 +0300

    initial commit

---
+++ IOS-Shopping-App/IOS-Shopping-AppUITests/IOS_Shopping_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  IOS_Shopping_AppUITestsLaunchTests.swift
+//  IOS-Shopping-AppUITests
+//
+//  Created by Mertcan Yılmaz on 2.11.2022.
+//
+
+import XCTest
+
+final class IOS_Shopping_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
